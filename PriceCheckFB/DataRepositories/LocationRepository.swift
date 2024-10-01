49efeac8d002dcad07acd77fa2756bad3dcc09bc
Grace <gracelia@andrew.cmu.edu>
2024-10-01 15:14:47 -0400

    Grace's changes

--- PriceCheckFB/DataRepositories/LocationRepository.swift
+++ PriceCheckFB/DataRepositories/LocationRepository.swift
@@ -6,26 +6,33 @@
 
 import Combine
 // import Firebase modules here
-
-
-
+import FirebaseCore
+import FirebaseFirestore
 
 class LocationRepository: ObservableObject {
   // Set up properties here
-  
-  
+    private let path: String = "location_scans"
+    private let store = Firestore.firestore()
+    
+    @Published var locations: [Location] = []
   
   init() {
-    get()
+      self.get()
   }
 
-  func get() {
-    // Complete this function
-    
-    
-    
-    
+    func get() {
+      store.collection(path)
+        .addSnapshotListener { querySnapshot, error in
+          if let error = error {
+            print("Error getting scans: \(error.localizedDescription)")
+            return
+          }
+
+          self.locations = querySnapshot?.documents.compactMap { document in
+            try? document.data(as: Location.self)
+          } ?? []
+        }
+    }
     
 
   }
-}
